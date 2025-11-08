0c9e79ee3d717a6b992dc39ecbdccdd3e4f576b6
Ericvf <ericvf@fegelein.com>
2025-11-09 00:49:56 +0100

    Tool

---
+++ UpnpTest/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UpnpTest;
+
+var serviceProvider = BuildServiceProvider();
+var app = serviceProvider.GetRequiredService<App>();
+await app.RunAsync();
+
+static ServiceProvider BuildServiceProvider()
+{
+    var services = new ServiceCollection();
+    services.AddSingleton<App>();
+    services.AddSingleton<SonosDiscoverService>();
+    services.AddHttpClient();
+
+    return services.BuildServiceProvider();
+}
+
+public class App
+{
+    private readonly SonosDiscoverService sonosDiscoverService;
+
+    public App(SonosDiscoverService sonosDiscoverService)
+    {
+        this.sonosDiscoverService = sonosDiscoverService;
+    }
+
+    public async Task<int> RunAsync()
+    {
+        Console.WriteLine("Started Sonos.Cli v1.0.0.0");
+        Console.WriteLine("Discovering devices...");
+
+        var deviceDiscovery = sonosDiscoverService.Discover(CancellationToken.None);
+        var devices = new List<SonosDevice>();
+        await foreach (var device in deviceDiscovery)
+        {
+            Console.WriteLine($"{device.FriendlyName} {device.RoomName}");
+            devices.Add(device);
+        }
+
+        Console.WriteLine();
+
+        var office = devices.FirstOrDefault(d => d.RoomName == "Office");
+        var vol = await sonosDiscoverService.GetVolume(office);
+
+        Console.WriteLine($"Room: {office.RoomName} - Volume: {vol}");
+
+        //await sonosDiscoverService.SetVolume(office, vol + 10);
+
+        var zone = await sonosDiscoverService.GetZone(office);
+        //var officeGroup = zone.ZoneGroups.FirstOrDefault(z => z.ZoneGroupMember.Any(m => m.UUID == office.UDN.Substring(5)));
+
+        //var coordinator = devices.FirstOrDefault(d => d.UDN.Substring(5) == officeGroup.Coordinator);
+        //var groupvol = await sonosDiscoverService.GetGroupVolume(coordinator);
+        //Console.WriteLine($"Group: {coordinator.DisplayName} - Volume: {groupvol}");
+
+        //await sonosDiscoverService.SetGroupVolume(coordinator, groupvol + 10);
+
+        foreach (var group in zone.ZoneGroups.OrderBy(z => z.ID))
+        {
+            Console.WriteLine($" Zone Group: {group.ID} (Coordinator: {group.Coordinator})");
+            foreach (var member in group.ZoneGroupMember.OrderBy(z => z.UUID))
+            {
+                Console.WriteLine($"  Member: {member.UUID} - {member.ZoneName} ({member.Location})");
+
+                if (member.Satellite != null)
+                {
+                    foreach (var sat in member.Satellite)
+                    {
+                        Console.WriteLine($"   Satellite: {sat.UUID} - {sat.ZoneName} ({sat.Location})");
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ UpnpTest/SonosDevice.cs
@@ -0,0 +1,47 @@
+﻿namespace UpnpTest
+{
+    public class SonosDevice
+    {
+        public string BaseUri { get; set; }
+        public string DeviceType { get; set; }
+        public string FriendlyName { get; set; }
+        public string Manufacturer { get; set; }
+        public string ManufacturerURL { get; set; }
+        public string ModelNumber { get; set; }
+        public string ModelDescription { get; set; }
+        public string ModelName { get; set; }
+        public string ModelURL { get; set; }
+        public string SoftwareVersion { get; set; }
+        public string SwGen { get; set; }
+        public string HardwareVersion { get; set; }
+        public string SerialNum { get; set; }
+        public string MACAddress { get; set; }
+        public string UDN { get; set; }
+        public string RoomName { get; set; }
+        public string DisplayName { get; set; }
+        public Dictionary<string, string> Versions { get; set; }
+        public List<Service> Services { get; set; }
+        public List<SonosDevice> SubDevices { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FriendlyName} ({RoomName}) - {ModelName} [{SerialNum}]";
+        }
+
+        public class Service
+        {
+            public string ServiceType { get; set; }
+            public string ServiceId { get; set; }
+            public string ControlURL { get; set; }
+            public string EventSubURL { get; set; }
+            public string SCPDURL { get; set; }
+
+            public override string ToString()
+            {
+                return $"{ServiceType} ({ServiceId})";
+            }
+        }
+
+
+    }
+}
